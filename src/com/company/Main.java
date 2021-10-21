58eca985986e353f3ff5dd5ad1ea74f4269ffa14
moin93 <moinmirmasomi@outlook.com>
2021-10-21 09:34:15 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //textEditAnonymousClasses textEditAnonymousClasses=new textEditAnonymousClasses();
+        //texteditcombobox texteditcombobox=new texteditcombobox();
+        //texteditinnerclasses texteditinnerclasses=new texteditinnerclasses();
+        //texteditlambdas texteditlambdas=new texteditlambdas();
+        texteditlocalklasses texteditlocalklasses=new texteditlocalklasses();
+    }
+}
